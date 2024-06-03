ae5b62ce35324651e91fe0a1909f515fa634e922
Yves Sorge <yves.sorge@studium.uni-hamburg.de>
2024-06-03 21:44:03 +0200

    feat: new parameters

--- ctd_tools/ctd_parameters.py
+++ ctd_tools/ctd_parameters.py
@@ -12,14 +12,17 @@ LATITUDE = 'latitude'
 LONGITUDE = 'longitude'
 DENSITY = 'density'
 POTENTIAL_TEMPERATURE = 'potential_temperature'
+SPEED_OF_SOUND = 'speed_of_sound'
 TIME_J = 'julian_days_offset'
 TIME_Q = 'seconds_since_jan_1_2000'
 TIME_N = 'timeN'
 TIME_S = 'timeS'
+POWER_SUPPLY_INPUT_VOLTAGE = 'power_supply_input'
 
+# Meta data should use standardized values from https://cfconventions.org/
 metadata = {
     TEMPERATURE: {
-        'long_name': "Sea Water Temperature",
+        'long_name': "Temperature",
         'units': "ITS-90, deg C",
         'coverage_content_type': 'physicalMeasurement',
         'standard_name': 'sea_water_temperature',
@@ -27,30 +30,37 @@ metadata = {
         'measurement_type': "Measured",
     },
     PRESSURE: {
+        'long_name': "Pressure",
+        'units': "dbar",
         'coverage_content_type': 'physicalMeasurement',
         'standard_name': 'sea_water_pressure',
         'short_name': "WP",
         'measurement_type': "Measured",
     },
     CONDUCTIVITY: {
+        'long_name': "Conductivity",
         'coverage_content_type': 'physicalMeasurement',
+        'units': "S m-1",
         'standard_name': 'sea_water_electrical_conductivity',
         'short_name': "COND",
         'measurement_type': "Measured",
     },
     SALINITY: {
+        'long_name': "Salinity",
         'coverage_content_type': 'physicalMeasurement',
         'standard_name': 'sea_water_salinity',
         'short_name': 'SAL',
         'measurement_type': 'Derived', 
     },
-    CONDUCTIVITY: {
+    TURBIDITY: {
+        'long_name': "Turbidity",
         'coverage_content_type': 'physicalMeasurement',
         'standard_name': 'sea_water_turbidity',
         'measurement_type': "Measured",
         'short_name': "Tur", 
     }, 
     OXYGEN: {
+        'long_name': "Oxygen",
         'coverage_content_type': 'physicalMeasurement',
         'standard_name': 'volume_fraction_of_oxygen_in_sea_water'
     },
@@ -74,6 +84,12 @@ metadata = {
         'standard_name': 'sea_water_potential_temperature',
         'measurement_type': 'Derived',
     },
+    SPEED_OF_SOUND: {
+        'long_name': 'Speed of Sound',
+        'units': 'm s-1',
+        'standard_name': 'speed_of_sound_in_sea_water',
+        'measurement_type': 'Derived',
+    },
     LATITUDE: {
         'long_name': 'Latitude',
         'units': 'degrees_north',
@@ -92,6 +108,10 @@ metadata = {
         'long_name': 'Time',
         'standard_name': 'time',
         'coverage_content_type': 'coordinate' 
+    },
+    POWER_SUPPLY_INPUT_VOLTAGE: {
+        'long_name': 'Power supply input voltage',
+        'units': 'V',
     }
 
 }
@@ -135,7 +155,10 @@ default_mappings = {
     ], 
     LONGITUDE: [
         'longitude', 'LATITUDE'
-    ]
+    ],
+    POWER_SUPPLY_INPUT_VOLTAGE: {
+        'Vbatt', 'Vcharge', 'Vmote'
+    }
 }
 
 def allowed_parameters():
